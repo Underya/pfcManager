54c0a8a2a047be88c69b3847b621a2ad585bd6ba
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-08-27 17:03:54 +0500

    Добавлен шаблон стринцы

---
+++ pfcManager/FoodList/FoodListVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pfcManager.FoodList
+{
+    /// <summary>
+    /// View-Model - 
+    /// </summary>
+    class FoodListVM : PropertyChanges
+    {
+        /// <summary>
+        /// Имя, по которому происходит сортировка
+        /// </summary>
+        string sortText = "test";
+
+        public string SortText
+        {
+            get
+            {
+                return sortText;
+            }
+            set
+            {
+                sortText = value;
+                OnPropertyChanged("SortText");
+            }
+        }
+    }
+}
---
+++ pfcManager/PropertyChanges.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Controls;
+using System.Runtime.CompilerServices;
+using pfcManager.Model;
+using pfcManager.FirstPage;
+using pfcManager.DayStatist;
+using pfcManager.FoodList;
+
+namespace pfcManager
+{
+    /// <summary>
+    /// Класс предоставляет методы для обновления своих свойств 
+    /// </summary>
+    class PropertyChanges : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Событие, вызываемое пр иобновлении
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Метод, через который происходит указание, что элемнет изменил значение
+        /// </summary>
+        /// <param name="prop"></param>
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
