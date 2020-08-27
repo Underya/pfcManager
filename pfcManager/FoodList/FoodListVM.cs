b02ce1227e84079bfc80df0cb798bbbfcb740519
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-08-27 17:24:08 +0500

    Добавлена привязка таблицы к данным

--- pfcManager/FoodList/FoodListVM.cs
+++ pfcManager/FoodList/FoodListVM.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using pfcManager.FirstPage;
+using pfcManager.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace pfcManager.FoodList
@@ -12,8 +17,11 @@ namespace pfcManager.FoodList
         /// <summary>
         /// Имя, по которому происходит сортировка
         /// </summary>
-        string sortText = "test";
+        string sortText = "";
 
+        /// <summary>
+        /// Текст, в соотвествии с которым исчутся блюда
+        /// </summary>
         public string SortText
         {
             get
@@ -24,6 +32,32 @@ namespace pfcManager.FoodList
             {
                 sortText = value;
                 OnPropertyChanged("SortText");
+            }
+        }
+
+        public ObservableCollection<FoodUpd> Foods
+        {
+            get
+            {
+                //Создание новой коллекции
+                ObservableCollection<FoodUpd> eatingUpdates = new ObservableCollection<FoodUpd>();
+
+                //Получение всей еды, подходящей под описание
+                using(ModelContext mc = new ModelContext())
+                {
+                    List<Food> foods = mc.Food.Where(obj => obj.Name.ToLower().Contains(sortText.ToLower())).ToList();
+                    //Перевод в другой массив и обёртка в производный класс
+                    foreach(Food food in foods)
+                    {
+                        eatingUpdates.Add(new FoodUpd(food));
+                    }
+                }
+
+                return eatingUpdates;
+            }
+            set
+            {
+
             }
         }
     }
---
+++ pfcManager/FoodList/FoodUpd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using pfcManager.Model;
+
+namespace pfcManager.FoodList
+{
+    /// <summary>
+    ///  Модель еды с автобновлением некоторых парамтеров
+    /// </summary>
+    class FoodUpd : Food
+    {
+        /// <summary>
+        /// Конструктор для трансформации из базового типа в обновляемый
+        /// </summary>
+        /// <param name="food"></param>
+        public FoodUpd(Food food)
+        {
+            //Сохранение всех параметров
+            this.Id = food.Id;
+            this.Name = food.Name;
+            this.Kkal = food.Kkal;
+            this.Description = food.Description;
+            this.Carbohydrates = food.Carbohydrates;
+            this.Eating = food.Eating;
+            this.Fats = food.Fats;
+            this.Protein = food.Protein;
+        }
+    }
+}
